e98f12c4b10bac0acd0edc2b4f548155aea49251
xuhaoran <xuhaoran@fclassroom.com>
2019-02-25 12:01:10 +0800

    引入dio 第三方物料库

--- sunny_shop/lib/pages/home_page.dart
+++ sunny_shop/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class HomePage extends StatelessWidget {
   final Widget child;
@@ -7,10 +8,25 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    getHttp();
     return Scaffold(
       body: Center(
-        child: Text('首页'),
+        child: Text('商城首页'),
       ),
     );
   }
+
+  void getHttp() async{
+    var url = 'https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=大胸妹子';
+    try {
+      Response response;
+      response =await Dio().get(url);
+      print(response);
+    } catch (e) {
+      return print(e);
+    }
+  }
+
+
 }
