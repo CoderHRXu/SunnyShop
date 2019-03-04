d094ea9537a1003f26c1099eb4a8c7967d30d016
xuhaoran <xuhaoran@fclassroom.com>
2019-03-04 11:59:46 +0800

    url_launcher 处理url

--- lib/widgets/LeaderPhone.dart
+++ lib/widgets/LeaderPhone.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LeaderPhone extends StatelessWidget {
   final String leaderImage;
@@ -11,10 +12,21 @@ class LeaderPhone extends StatelessWidget {
     return Container(
       child: InkWell(
         child: Image.network(leaderImage),
-        onTap: (){
-          
-        },
+        onTap: _launchURL,
+        
       ),
     );
   }
+
+
+  void _launchURL() async {
+    // String url = 'tel:' + leaderPhone;
+    String url = "https://www.baidu.com";
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw url + '此url不能访问';
+    }
+  }
+
 }
