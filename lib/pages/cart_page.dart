f39a41ac8ea936424e7076c62086e57dc5bfdfec
xuhaoran <xuhaoran@fclassroom.com>
2019-04-21 14:36:22 +0800

    provider 初次使用

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provide/provide.dart';
+import '../provide/counter.dart';
 
 class CartPage extends StatelessWidget {
   final Widget child;
@@ -8,9 +10,49 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('购物车'),
+      body: Container(
+        child: Row(
+          children: <Widget>[
+            Number(),
+            MyButton()
+          ],
+        )
       ),
+    );
+  }
+
+}
+
+
+class Number extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 200),
+      child: Provide<Counter>(
+        builder: (context, child, counter){
+          return Text(
+            '${counter.value}', 
+            style:Theme.of(context).textTheme.display1,
+            );
+        },
+      ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: RaisedButton(
+        onPressed: (){
+          // 直接编写按钮的单击事件，并调用provide里的方法，代码修改如下。
+          Provide.value<Counter>(context).increment();
+        },
+        child: Text('递增按钮'),
+      )
     );
   }
 }
